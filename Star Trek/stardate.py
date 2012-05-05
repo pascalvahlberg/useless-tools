ff12f70cbc54c35839a9272237aa892a7088d840
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-05-05 15:17:59 +0200

    [4] Other/Checksum: 85caa8eb82f196f384e8d787165a506b
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- Star Trek/stardate.py
+++ Star Trek/stardate.py
@@ -2,10 +2,15 @@
 
 from sys import exit
 from time import time
+from gtk import Clipboard
 
 stardate_raw = str(time())
 stardate = stardate_raw[1:6] + "," + stardate_raw[6]
 
 print(stardate)
+
+cb = Clipboard()
+cb.set_text(stardate)
+cb.store()
 
 exit(0)
