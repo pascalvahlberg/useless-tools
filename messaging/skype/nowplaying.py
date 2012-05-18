6151ec7df07dbd68ff1ad2081e8aecca33745463
Chiruclan <administrator@chiruclan.de>
2012-05-18 21:41:19 +0200

    [18] Other/Checksum: 65a272630325029e3dca48340fbc0aef
    Signed-off-by: Chiruclan <administrator@chiruclan.de>

--- messaging/skype/nowplaying.py
+++ messaging/skype/nowplaying.py
@@ -3,69 +3,70 @@
 import Skype4Py
 from urllib2 import urlopen
 from time import sleep
-from sys import exit
+from sys import argv, exit
 
-try:
-	skype = Skype4Py.Skype()
-	skype.Attach()
-	oldmood = skype.Profile("MOOD_TEXT")
-except Exception,e:
-	print(e)
-	exit(0)
-except KeyboardInterrupt:
-	print("")
-	print("CTRL + C")
-	exit(0)
+if len(argv) == 2:
+	path = argv[1]
 
-print("Enter the path or the url to your song file:")
-path = raw_input()
-if path.startswith("http://"):
-	print("Okay: It's an url!")
-	song = ""
-	while 1:
-		try:
-			getsong = urlopen(path).read().rstrip()
-			if song != getsong:
-				song = getsong
-				skype.Profile("MOOD_TEXT", unicode(song) + " (Now-Playing from Chiruclan)")
-			sleep(2)
-		except Exception,e:
-			print(e)
-			pass
-		except KeyboardInterrupt:
-			print("")
-			print("CTRL + C")
-			skype.Profile("MOOD_TEXT", oldmood)
-			exit(0)
+	try:
+		skype = Skype4Py.Skype()
+		skype.Attach()
+		oldmood = skype.Profile("MOOD_TEXT")
+	except Exception,e:
+		print(e)
+		exit(0)
+	except KeyboardInterrupt:
+		print("")
+		print("CTRL + C")
+		exit(0)
+
+	if path.startswith("http://"):
+		print("Okay: It's an url!")
+		song = ""
+		while 1:
+			try:
+				getsong = urlopen(path).read().rstrip()
+				if song != getsong:
+					song = getsong
+					skype.Profile("MOOD_TEXT", unicode(song.encode('string_escape')).encode('utf-8') + " (Now-Playing from Chiruclan)")
+				sleep(2)
+			except Exception,e:
+				print(e)
+				pass
+			except KeyboardInterrupt:
+				print("")
+				print("CTRL + C")
+				skype.Profile("MOOD_TEXT", oldmood)
+				exit(0)
+	else:
+		print("Okay: It's a file!")
+		song = ""
+		while 1:
+			try:
+				getsong = file(path).read().rstrip()
+				if song != getsong:
+					song = getsong
+					skype.Profile("MOOD_TEXT", unicode(song.encode('string_escape')).encode('utf-8') + " (Now-Playing by Chiruclan)")
+				sleep(2)
+			except Exception,e:
+				print(e)
+				pass
+			except KeyboardInterrupt:
+				print("")
+				print("CTRL + C")
+				skype.Profile("MOOD_TEXT", oldmood)
+				exit(0)
+
+	try:
+		skype.Profile("MOOD_TEXT", oldmood)
+	except Exception,e:
+		print(e)
+		exit(0)
+	except KeyboardInterrupt:
+		print("")
+		print("CTRL + C")
+		exit(0)
 else:
-	print("Okay: It's a file!")
-	song = ""
-	while 1:
-		try:
-			getsong = file(path).read().rstrip()
-			if song != getsong:
-				song = getsong
-				skype.Profile("MOOD_TEXT", unicode(song) + " (Now-Playing by Chiruclan)")
-			sleep(2)
-		except Exception,e:
-			print(e)
-			pass
-		except KeyboardInterrupt:
-			print("")
-			print("CTRL + C")
-			skype.Profile("MOOD_TEXT", oldmood)
-			exit(0)
-
-try:
-	skype.Profile("MOOD_TEXT", oldmood)
-except Exception,e:
-	print(e)
-	exit(0)
-except KeyboardInterrupt:
-	print("")
-	print("CTRL + C")
-	exit(0)
-
-print("Exiting...")
+	print("NOWPLAYING.PY <path/url>")
 	
 exit(0)
